91effdf0e42d6774715362234dcce5c687d28242
ayushg-1506 <ayushgupta01506@gmail.com>
2025-12-10 00:23:09 +0530

    js(update)

--- cpp/practice.cpp
+++ cpp/practice.cpp
@@ -2,50 +2,33 @@
 using namespace std;
 
 int main() {
-	int t;
-	cin >>t;
-	vector<string> r;
-	while(t--){
-	    int n,k;
-	    cin >> n >> k;
-	    string s;
-	    cin >> s;
-	    int a=0,b=0;
-	    for(int i=0;i<n;i++){
-	        if(s[i] == '0'){
-	            a++;
-	        }
-	        else{
-	            b++;
-	        }
-	    }
-		if(k ==1){
-			if(a==b || a == b-1 || a-1 ==b){
-				r.push_back("YES");
-				continue;
-			}
-		}
-	    int c = n-k;
-	    int d = min(a,b);
-		if(n%2 == 0){
-			if(d >= c){
-				r.push_back("YES");
-			}
-			else{
-				r.push_back("NO");
-			}
-	   }
-	   if(n%2 != 0){
-			if(d >= c-1){
-				r.push_back("YES");
-			}
-			else{
-				r.push_back("NO");
-			}
-		}
-	}
-	for( string v:r){
-	    cout << v << endl;
-	}
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int n;
+    cin >> n;
+
+    while (n--) {
+        int p, k;
+        cin >> p >> k;
+
+        string s;
+        cin >> s;  // read "1001"
+
+        int c = 0;
+        int i = 0;
+
+        while (i < p) {
+            if (s[i] == '0') {
+                c++;
+                i++;
+            } else {   // s[i] == '1'
+                i += k + 1;
+            }
+        }
+
+        cout << c << "\n";
+    }
+
+    return 0;
 }
